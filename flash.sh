4eaa6a4ebf73fa4365231699b8c3c010e5c0557d
mathiaslyngbye <malyn16@student.sdu.dk>
2019-10-14 12:01:28 +0200

    Add establish connection parameter

--- flash.sh
+++ flash.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-if [ "$1" = "--run" ] || [ "$1" = "-r" ];
+
+
+if [ "$1" = "--login" ] || [ "$1" = "-l" ];
+then
+    echo "Attempting ssh connection...";
+    sshpass -p 'ai1rockz' ssh ai1@192.168.0.1
+elif [ "$1" = "--run" ] || [ "$1" = "-r" ];
 then
     echo "Running ev3control.py remotely...";
     # Run program
     sshpass -p 'ai1rockz' ssh ai1@192.168.0.1 python3 ev3control.py
 else
-    echo "Flashing ev3control.py...";
+    echo "Flashing ev3control.pyi and instructions.csv...";
     # Use sshpass to single command copy main file
     sshpass -p 'ai1rockz' scp ./ev3control.py ai1@192.168.0.1:~/
+    sshpass -p 'ai1rockz' scp ./instructions.csv ai1@192.168.0.1:~/
 fi
 
 
