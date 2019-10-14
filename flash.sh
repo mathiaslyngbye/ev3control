287cf5c6ddd47b11b817d84d0d4d9ca8a26d6683
mathiaslyngbye <malyn16@student.sdu.dk>
2019-10-14 10:33:35 +0200

    Add copy and run functionality

--- flash.sh
+++ flash.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+if [ "$1" = "--run" ] || [ "$1" = "-r" ];
+then
+    echo "Running ev3control.py remotely...";
+    # Run program
+    sshpass -p 'ai1rockz' ssh ai1@192.168.0.1 python3 ev3control.py
+else
+    echo "Flashing ev3control.py...";
+    # Use sshpass to single command copy main file
+    sshpass -p 'ai1rockz' scp ./ev3control.py ai1@192.168.0.1:~/
+fi
 
-scp ./ev3control.py ai1@192.168.0.1:~/ev3control/
+
